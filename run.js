05dfe4cdf834622f3a9f927e71284a334c9180f8
x_false <514286291@qq.com>
2020-09-24 16:57:42 +0800

    🚑  添加 时间 测试

--- run.js
+++ run.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 
 const crawler = require('./crawler')
 
-const task = cron.schedule('* * * * * *', () => {
+const task = cron.schedule('* 30 18 * * *', () => {
   console.log('tsak start -------->');
   crawler.crawler()
 });
