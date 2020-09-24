815374c2fab67e5810424e6bba85e8a0cb5cc027
x_false <514286291@qq.com>
2020-09-24 16:24:56 +0800

    🐛  提交 修改

--- run.js
+++ run.js
@@ -3,6 +3,7 @@ const cron = require('node-cron');
 const crawler = require('./crawler')
 
 const task = cron.schedule('* 2 * * * *', () => {
+  console.log('tsak start ');
   crawler.crawler()
 });
 
