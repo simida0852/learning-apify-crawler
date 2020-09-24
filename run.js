6780c7c590b803b4e60b0117f40c20a5ca4bf6f2
x_false <514286291@qq.com>
2020-09-24 16:32:53 +0800

    ✅

--- run.js
+++ run.js
@@ -2,8 +2,8 @@ const cron = require('node-cron');
 
 const crawler = require('./crawler')
 
-const task = cron.schedule('* 2 * * * *', () => {
-  console.log('tsak start ');
+const task = cron.schedule('* * * * * *', () => {
+  console.log('tsak start -------->');
   crawler.crawler()
 });
 
